c491d28da1a24eb345426aedc2d3fdafca10a592
arqdsis <arqdsis@MOOLIi0336.animaedu.intranet>
2019-10-05 16:34:53 -0300

    Atividade 1 - Concluída com sucesso/ Proximo passo fazer o mesmo processo na atividade 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,5 +10,26 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  contatos = [
+    {
+    nome: 'Dona Florinda',
+    telefone:'12345678',
+    email: 'd.florinda@gmail.com'
+    
+    },
+    {
+    nome: 'Madruga',
+    telefone: '11958013358',
+    email: 'madruguinha@gmail.com'
+    },
+    {
+    nome: 'Kiko',
+    telefone: '1112521984',
+    email: 'kiko@gmail.com'
+    }
+    ]
+    onItemClick (contato){
+      console.log("item-click", contato)
+      }
+      
 }
